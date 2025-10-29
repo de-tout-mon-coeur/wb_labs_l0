c179d652d30c96d82dd026ff2ed776b30d55569b
whatislove <40031147+de-tout-mon-coeur@users.noreply.github.com>
2025-10-29 22:58:36 +0700

    Delete demo-order/demo-order directory

--- demo-order/demo-order/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS orders (
-  order_uid TEXT PRIMARY KEY,
-  track_number TEXT,
-  data JSONB NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT now()
-);
-
-CREATE INDEX IF NOT EXISTS idx_orders_track ON orders(track_number);
